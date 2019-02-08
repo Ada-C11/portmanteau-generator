5d9cc8ed8b30609494bc6603a2ce75a292b077f7
Nene <36690142+elisepham37@users.noreply.github.com>
2019-02-08 10:01:21 -0800

    Create generator.rb

---
+++ generator.rb
@@ -0,0 +1,71 @@
+def is_vowel?(letter)
+  vowels = ["a", "e", "i", "o", "u"]
+  return vowels.include?(letter)
+end
+
+def is_valid(input)
+  result = true
+  if input.length < 2
+    puts "Your input should be at least two characters."
+    result = false
+  end
+
+  if input !~ /[aeiou]/
+    puts "Your input should contain at least one vowel."
+    result = false
+  end
+
+  return result
+end
+
+def get_input
+  input = gets.chomp.downcase
+  until is_valid(input)
+    puts "Please enter a different word: "
+    input = gets.chomp.downcase
+  end
+  return input
+end
+
+def create_first_half(word)
+  last_vowel = -1
+  word.each_char { |c|
+    if is_vowel?(c)
+      last_vowel = word.rindex(c)
+    end
+  }
+
+  if last_vowel == -1
+    return word
+  end
+  return word.slice(0...last_vowel)
+end
+
+def create_second_half(word)
+  word.each_char { |c|
+    if is_vowel?(c)
+      i = word.index(c)
+      return word.slice(i...word.length)
+    end
+  }
+  return word
+end
+
+def run_generator
+  puts "Please enter a word to begin the portmanteau: "
+  first_word = get_input
+
+  puts "Please enter a word to end the portmanteau: "
+  second_word = get_input
+
+  first_half = create_first_half(first_word)
+  second_half = create_second_half(second_word)
+  puts "#{first_word} + #{second_word} is #{first_half + second_half}"
+end
+
+answer = "yes"
+until answer == "no"
+  run_generator
+  puts "Do you want to keep playing (Yes or No)? "
+  answer = gets.chomp.downcase
+end
