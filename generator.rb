863f508b9e1b87b3e5ca62db31e86a1791402b2d
MariaWissler <31623564+MariaWissler@users.noreply.github.com>
2019-02-08 10:34:25 -0800

    Create generator.rb

---
+++ generator.rb
@@ -0,0 +1,10 @@
+# method is a vowel ?
+# user input 
+#  split the word and compare to see if a vowel, from 0 to lenght, push to store
+#  split second word compare to see if is a vowel starting at -1 and store 
+#  put this two values together  
+def includes_vowel (word)
+ word.each_char   
+end 
+
+puts 
