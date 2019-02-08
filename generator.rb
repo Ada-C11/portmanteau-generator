c0ca8ef6d9f4386b347e07b4c765c4c3e86db60b
Dan Roberts <dan@adadevelopersacademy.org>
2019-02-08 10:21:36 -0800

    Create generator.rb

---
+++ generator.rb
@@ -0,0 +1,18 @@
+my_name = "Dan"
+
+puts my_name
+
+10.times do
+  greeting = "Hello #{my_name}"
+  puts greeting
+end
+
+# puts greeting
+
+conditional_variable = "default"
+
+if 3 == 3
+  conditional_variable = "It worked"
+end
+
+puts conditional_variable
