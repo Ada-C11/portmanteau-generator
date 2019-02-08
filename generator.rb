3754987f298787509ad568cfafdec39e3e14e106
Angela <40902778+AngelaOh@users.noreply.github.com>
2019-02-08 10:01:21 -0800

    Create generator.rb

---
+++ generator.rb
@@ -0,0 +1,104 @@
+def is_vowel?(letter)
+  vowel_array = ["a", "e", "i", "o", "u"]
+  if vowel_array.include? letter.downcase
+    # if letter == "a" || letter == "e" || letter == "i" || letter == "o" || letter == "u"
+    return true
+  else
+    return false
+  end
+end
+
+def run_generator
+  print "Please enter the first word: "
+  wordA = gets.chomp
+
+  #make sure each input for wordA and wordB is two or more characters
+  until wordA.length >= 2
+    print "Please make sure your word is at least two characters long. Try again: "
+    wordA = gets.chomp
+  end
+
+  print "Please enter the second word: "
+  wordB = gets.chomp
+
+  until wordB.length >= 2
+    print "Please make sure your word is at least two characters long. Try again: "
+    wordB = gets.chomp
+  end
+
+  #wordA: iterate through word backwards and find the index for the first vowel
+  index = wordA.length - 1
+  vowels_tracker = 0
+  ## tried attaching downcase method to line 54 but didn't allow .each_char method
+  # wordA = wordA.downcase || use if you want output to be lowercased
+  wordA.reverse.each_char do |letter|
+    if is_vowel?(letter) == false && vowels_tracker < 1
+      index -= 1
+    else
+      vowels_tracker += 1
+    end
+  end
+
+  ## OLD CODE
+  #iterate through wordA and store until (excluding) that index
+  # first_half = ""
+  # second_index = 0
+  # wordA.each_char do |letter|
+  #   if second_index < index
+  #     first_half << letter
+  #     second_index += 1
+  #   end
+  # end
+
+  first_half = wordA[0...index]
+
+  #wordB: iterate through word and find index for first vowel
+  index = 0
+  vowels_tracker = 0
+  ## tried attaching downcase method to line 54 but didn't allow .each_char method
+  # wordB = wordB.downcase || use if you want output to be lowercased
+  wordB.each_char do |letter|
+    if is_vowel?(letter) == false && vowels_tracker < 1
+      index += 1
+    else
+      vowels_tracker += 1
+    end
+  end
+
+  #store word starting from that index
+  second_half = ""
+
+  ## OLD CODE
+  # second_index = 0
+  # #accounts for words with zero vowels
+  # if index >= wordB.length
+  #   second_half = wordB
+  #   #stores letters until first vowel is hit (including)
+  # else
+  #   wordB.each_char do |letter|
+  #     if second_index >= index
+  #       second_half << letter
+  #       second_index += 1
+  #     else
+  #       second_index += 1
+  #     end
+  #   end
+  # end
+
+  second_half = wordB[index..wordB.length - 1]
+
+  #add both words together and display
+  portmanteau = first_half + second_half
+  puts "Your words (#{wordA}) and (#{wordB}) turn into (#{portmanteau})!\n\n"
+end
+
+puts "Hello and welcome to the Portmanteau Generator program! You will be asked to enter two words and we will generate a portmanteau for you."
+
+continue_game = "Y"
+while continue_game == "Y"
+  run_generator
+
+  #option for user to continue or end program.
+  print "Continue? (Y/N): "
+  continue_game = gets.chomp.upcase!
+end
