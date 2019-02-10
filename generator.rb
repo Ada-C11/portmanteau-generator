c06d2f3a04f0a49400356c134b925245417af73e
amythetester <36608155+amythetester@users.noreply.github.com>
2019-02-10 01:15:17 -0800

    Update generator.rb

--- generator.rb
+++ generator.rb
@@ -10,7 +10,7 @@ def input_validation
     # request user input
     input = gets.chomp
 
-    #word length validation
+    # word length validation
     until input.length > 1
         puts "Please enter more than one character."
         input = gets.chomp
@@ -18,7 +18,7 @@ def input_validation
     return input.downcase
 end
 
-# finds last vowel of first word
+# finds last vowel of a word
 def last_vowel(word)
     i = 0
     last_vowel = word.length
@@ -29,11 +29,12 @@ def last_vowel(word)
         end
         i += 1 
     end
+    # returns word from first letter to letter before last vowel
     # note for dee ==> I assumed that if the first letter of the first word is a vowel that the whole first word would be used
     return word[0..last_vowel-1]
 end
 
-# finds first vowel of second word
+# finds first vowel of a word
 def first_vowel(word)
     i = 0
     first_vowel = 0
@@ -45,6 +46,7 @@ def first_vowel(word)
         end
         i += 1 
     end
+    # returns word from first vowel to the last letter
     return word[first_vowel..word.length]
 end
 
@@ -67,10 +69,10 @@ def run_generator
     puts "The second input is #{word_b}"
     puts "in the run_generator method"
 
-    # returns word from first letter to letter before last vowel
+    # returns first word from first letter to letter before last vowel 
     word1 = last_vowel(word_a)
 
-    # returns word from first vowel to the last letter
+    # returns second word from first vowel to the last letter
     word2 = first_vowel(word_b)
 
     # generates portmanteau
@@ -82,7 +84,3 @@ end
 
 # runs generator
 puts run_generator
-
-
-
-  
