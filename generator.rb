a8ba2ee859a908e16a28a9f3cc4dce652b29729a
kanderson38 <46275153+kanderson38@users.noreply.github.com>
2019-02-09 20:19:35 -0800

    Update generator.rb

--- generator.rb
+++ generator.rb
@@ -1,3 +1,24 @@
+def prompt_for_input(which_word)
+  continue = false
+
+  until continue == true
+    print "Please enter the #{which_word} word: "
+    word = gets.chomp
+
+    until word.length > 1
+      print "Please enter a word with more than one character: "
+      word_a = gets.chomp
+    end
+
+    continue = validate(word)
+    if continue == false
+      puts "The word must contain at least one vowel."
+    end
+  end
+
+  return word
+end
+
 def is_vowel?(letter)
   case letter
   when "a", "e", "i", "o", "u", "A", "E", "I", "O", "U"
@@ -9,39 +30,39 @@ end
 
 def find_vowels(word)
   # create an array to hold the indices of all the vowels in word_a
-  # needs to include -1 in case there are no vowels in the first word (to include the entirety of the first word in the portmanteau)
-  vowel_array = [-1]
+
+  vowel_array = []
 
   #   find each vowel in the word and add its index to the array
-  word.each_char.with_index { |c, i|
-    if is_vowel?(c)
+  # word.each_char.with_index { |c, i|
+  #   if is_vowel?(c)
+  #     vowel_array << i
+  #   end
+  # }
+
+  temp_array = word.split("")
+  temp_array.length.times do |i|
+    if is_vowel?(temp_array[i])
       vowel_array << i
     end
-  }
-
-  #   if no vowels were found, add a 0 to the array so word_b will be sliced properly
-  if vowel_array == [-1]
-    vowel_array << 0
   end
+
   return vowel_array
 end
 
+def validate(word)
+  vowel_array = find_vowels(word)
+  if vowel_array == []
+    return false
+  else
+    return true
+  end
+end
+
 def run_generator
-  print "Please enter the first word: "
-  word_a = gets.chomp
+  word_a = prompt_for_input("first")
 
-  until word_a.length > 1
-    print "Please enter a word with more than one character: "
-    word_a = gets.chomp
-  end
-
-  print "Please enter the second word: "
-  word_b = gets.chomp
-
-  until word_b.length > 1
-    print "Please enter a word with more than one character: "
-    word_b = gets.chomp
-  end
+  word_b = prompt_for_input("second")
 
   vowel_array_a = find_vowels(word_a)
 
@@ -51,15 +72,28 @@ def run_generator
 
   # slice the string before the first vowel for the second word
   vowel_array_b = find_vowels(word_b)
-  slice_point = vowel_array_b[1]
+
+  slice_point = vowel_array_b[0]
   word_b_piece = word_b.slice((slice_point)..word_b.length)
 
   portmanteau = word_a_piece + word_b_piece
   return portmanteau
 end
 
+run_again = "y"
 puts "Welcome to the portmanteau generator!"
-portmanteau = run_generator
 
-puts "The portmanteau is #{portmanteau.downcase}."
+while run_again == "y"
+  portmanteau = run_generator
+
+  puts "The portmanteau is #{portmanteau.downcase}."
+
+  print "Would you like to make another portmanteau (y/n)? "
+  run_again = gets.chomp
+  while run_again != "y" && run_again != "n"
+    print "Please enter \"y\" for yes or \"n\" for no. "
+    run_again = gets.chomp
+  end
+end
+
 puts "Thank you for using the portmanteau generator."
