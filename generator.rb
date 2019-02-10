aacef89daf02b361fe0f44d7e0e2e4dc633cd916
MariaWissler <31623564+MariaWissler@users.noreply.github.com>
2019-02-09 22:45:25 -0800

    Update generator.rb

--- generator.rb
+++ generator.rb
@@ -1,10 +1,29 @@
-# method is a vowel ?
-# user input 
-#  split the word and compare to see if a vowel, from 0 to lenght, push to store
-#  split second word compare to see if is a vowel starting at -1 and store 
-#  put this two values together  
-def includes_vowel (word)
- word.each_char   
+def get_the_vowel(word)
+  word.length > 1 && word =~ /[aeiou]/
 end 
 
-puts 
+puts "Please enter your first word"
+first_word = gets.chomp.downcase
+ until get_the_vowel(first_word)
+  puts "Please enter a word with 2 or more characters and a vowel"
+  first_word = gets.chomp.downcase
+  end
+ 
+words = []
+a = first_word.rindex(/[aeiou].*/)
+word_a = first_word[0...a]
+words << word_a
+
+puts "Please enter your second word"
+second_word = gets.chomp.downcase
+  until get_the_vowel(second_word)
+  puts "Please enter a word with 2 or more characters and a vowel"
+  second_word = gets.chomp.downcase
+  end 
+
+b = second_word.split(/([aeiou].*)/)
+word_b = b[1]
+words << word_b
+
+puts "Word #{word_a} + #{word_b} is #{words.join("")}"
+end
