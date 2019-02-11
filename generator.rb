e6e1648e135778305ec3c575135d0d1a8733486e
Jansen Martin <v.jansen.martin@gmail.com>
2019-02-11 12:10:01 -0800

    Update generator.rb

--- generator.rb
+++ generator.rb
@@ -29,57 +29,71 @@ end
 #   3) Finds and records the FIRST instance of a vowel in the second word
 #   4) Slices and combines both words based on the rules outlined in the assignment
 def run_generator
-  # Collects two word from the user
-  first_word = ask("\nWhat is the first word to combine?")
-  second_word = ask("\nWhat is the second character to combine?")
+  while true
+    # Collects two word from the user
+    first_word = ask("\nWhat is the first word to combine?")
+    second_word = ask("\nWhat is the second character to combine?")
 
-  # Finds the last instance of a vowel in the first word, then records that vowel's index number.
-  i = 0
-  index_of_vowel = 0
-  includes_vowel = false
-  first_word.each_char do |letter|
-    vowel = is_vowel?(letter)
+    # Finds the last instance of a vowel in the first word, then records that vowel's index number.
+    i = 0
+    index_of_vowel = 0
+    includes_vowel = false
+    first_word.each_char do |letter|
+      vowel = is_vowel?(letter)
 
-    if vowel
-      index_of_vowel = i
-      includes_vowel = true
+      if vowel
+        index_of_vowel = i
+        includes_vowel = true
+      end
+
+      i += 1
     end
 
-    i += 1
-  end
+    # If the first word doesn't include a vowel, then it's left unchanged.
+    # Otherwise, assigns every letter of first word -- up to and exluding the last vowel -- to "first_half"
+    if !includes_vowel
+      first_half = first_word
+    else
+      first_half = first_word.slice(0...index_of_vowel)
+    end
 
-  # If the first word doesn't include a vowel, then it's left unchanged.
-  # Otherwise, assigns every letter of first word -- up to and exluding the last vowel -- to "first_half"
-  if !includes_vowel
-    first_half = first_word
-  else
-    first_half = first_word.slice(0...index_of_vowel)
-  end
+    # Finds the first instance of a vowel in the second word, then records the vowel's index number
+    index_of_vowel = 0
+    includes_vowel = false
+    second_word.each_char do |letter|
+      vowel = is_vowel?(letter)
+      if vowel
+        includes_vowel = true
+        break
+      end
 
-  # Finds the first instance of a vowel in the second word, then records the vowel's index number
-  index_of_vowel = 0
-  includes_vowel = false
-  second_word.each_char do |letter|
-    vowel = is_vowel?(letter)
-    if vowel
-      includes_vowel = true
+      index_of_vowel += 1
+    end
+
+    # If the second word doesn't include a vowel, then it's left unchanged.
+    # Otherwise, assigns every letter after -- and including -- the vowel to "second_half"
+    if !includes_vowel
+      second_half = second_word
+    else
+      second_half = second_word.slice(index_of_vowel..second_word.length)
+    end
+
+    # Combines both halves into a portmanteau
+    portmanteau = first_half + second_half
+    puts portmanteau
+    
+    # Asks user if they want to make another portmanteau
+    print "\nMake another portmanteau? (yes/no) "
+    input = gets.chomp.downcase
+
+    # Program continues if input equals "yes"; Otherwise, program ends
+    if input == "yes"
+      puts "Okay!"
+    else
+      puts "Goodbye!"
       break
     end
-
-    index_of_vowel += 1
   end
-
-  # If the second word doesn't include a vowel, then it's left unchanged.
-  # Otherwise, assigns every letter after -- and including -- the vowel to "second_half"
-  if !includes_vowel
-    second_half = second_word
-  else
-    second_half = second_word.slice(index_of_vowel..second_word.length)
-  end
-
-  # Combines both halves into a portmanteau
-  portmanteau = first_half + second_half
-  puts portmanteau
 end
 
 run_generator
