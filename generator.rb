abd99d9e65abf657625877afba6ed0bbaea62038
msfinnan <35827624+msfinnan@users.noreply.github.com>
2019-02-10 19:42:58 -0800

    Update generator.rb

--- generator.rb
+++ generator.rb
@@ -1,6 +1,6 @@
-# portmanteau-generator 
+# portmanteau-generator
 
-# Define is_vowel?
+# Define method that checks if a letter is a vowel
 def is_vowel?(letter)
   if "aeiou".include?(letter)
     return true
@@ -9,56 +9,100 @@ def is_vowel?(letter)
   end
 end
 
-# TO DO : check case of input 
+# Define method that checks if a word contains a vowel
+def includes_vowel?(word)
+  if word.include?("a") || word.include?("e") || word.include?("i") || word.include?("o") || word.include?("u")
+    return true
+  else
+    return false
+  end
+end
+
+# Define method that runs the portmanteau generator
+# Get words to make into a portmanteau from user & check that the words are at least 2 characters long
 def run_generator
   puts "What is the first word to combine?"
-  first_word = gets.chomp
+  first_word = gets.chomp.downcase
   while first_word.length < 2
     puts "Enter a word that is at least 2 characters in length"
-    first_word = gets.chomp
+    first_word = gets.chomp.downcase
   end
 
-  # iterate through first word character
-  # create new variable to store shortened word
+  # Create new empty variable to store shortened first word (first half of portmanteau)
   new_first_word = ""
-  first_index = first_word.length - 1 #(length of the index is word length - 1 since index starts at 0)
-  first_word.reverse! #(reverse the string & make it stay reversed)
 
-  # TO DO ADD CONDITIONAL FOR NO VOWELS
-  first_word.each_char do |character|
-    if !is_vowel?(character)
-      first_index -= 1 # if it is not a vowel, subtract 1 from the index
-    else #(if it finds a vowel)
-      first_word.reverse! #(put the word back to correct order)
-      new_first_word = first_word[0...first_index] #(store the new shortened word in the new variable)
-      break # exit the loop
+  # Create index for first word (to use to cut word)
+  # (length of the index is string length - 1 since index starts at 0)
+  first_index = first_word.length - 1
+
+  # Reverse the first word string and keep it reversed
+  first_word.reverse!
+
+  # Check if first word contains a vowel 
+  # If it doesn't contain a vowel, leave as is
+  # If it does contain a vowel iterate through the letters in the string.
+  # Put every letter of the first word until and excluding its last vowel in the new variable
+  first_include_vowel_result = includes_vowel?(first_word)
+  if first_include_vowel_result == true
+    first_word.each_char do |character|
+      if !is_vowel?(character)
+        first_index -= 1
+      else
+        first_word.reverse!
+        new_first_word = first_word[0...first_index]
+        break
+      end
     end
+  else
+    new_first_word = first_word.reverse!
   end
 
+  # Get second word from user
+  # Check if has at least 2 characters
   puts "What is the second word to combine?"
-  second_word = gets.chomp
+  second_word = gets.chomp.downcase
   while second_word.length < 2
     puts "Enter a word that is at least 2 characters in length"
-    second_word = gets.chomp
+    second_word = gets.chomp.downcase
   end
 
-  # iterate through the second
-  # create variable to store shortened word
+  # Check if second word contains a vowel 
+  # If it doesn't contain a vowel, leave as is
+  # If it does contain a vowel iterate through the letters in the string.
+  # Add letters after and including its first vowel to the new variable 
   new_second_word = ""
   second_index = 0
-
-  second_word.each_char do |character|
-    if !is_vowel?(character)
-      second_index += 1
-    else
-      new_second_word = second_word[second_index..second_word.length]
-      break
+  second_include_vowel_result = includes_vowel?(second_word)
+  if second_include_vowel_result == true
+    second_word.each_char do |character|
+      if !is_vowel?(character)
+        second_index += 1
+      else
+        new_second_word = second_word[second_index..second_word.length]
+        break
+      end
     end
+  else
+    new_second_word = second_word
   end
-  # Output using slice method with index found in variables above
+
+  # Output original words and new portmanteau
   puts "First word is #{first_word}"
   puts "Second word is #{second_word}"
   puts "The portmanuteau is #{new_first_word}#{new_second_word}"
 end
 
+# Run method
 run_generator
+
+# Ask user if they want to run program again
+puts "Do you want to make another portmanteau? yes/no"
+answer = gets.chomp.downcase
+
+# Keep running program until user chooses to exit
+while answer == "yes"
+  run_generator
+  puts "Do you want to make another portmanteau? yes/no"
+  answer = gets.chomp.downcase
+  break if answer == "no" || answer == "exit"
+end
