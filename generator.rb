e583a06ac149110795b62af13ca4a166ff1cb8db
aphunk <41399466+aphunk@users.noreply.github.com>
2019-02-10 16:11:26 -0800

    Update generator.rb

--- generator.rb
+++ generator.rb
@@ -1,8 +1,3 @@
-# portmanteau assignment
-
-# ** include code for input without vowels
-
-# Defining a method to identify vowels
 def is_vowel?(letter)
   if "aeiou".include?(letter)
     return true
@@ -21,48 +16,42 @@ def run_generator
     word_a = gets.chomp.downcase
   end
 
-  # iterate through first word character
-  # initialize variable to store new truncated word
+  # Truncate word_a: all letters UNTIL and EXCLUDING the last vowel
+  # Check for vowels
+  word_a.reverse!
   new_word_a = ""
-  first_index = word_a.length - 1 # length of the index is word length - 1
-  word_a.reverse! # reverse the string. ! makes it stay reversed
-
-  # loop through to word to find vowel + index
-  word_a.each_char do |character|
-    if !is_vowel?(character)
-      first_index -= 1 # if it is not a vowel, subtract 1 from the index
+  word_a.each_char.with_index do |letter, index|
+    if is_vowel?(letter) == true
+      new_word_a = "#{word_a[(index + 1)..-1]}"
+      new_word_a = new_word_a.reverse!
+      break
     else
-      word_a.reverse! # de-reverse word
-      new_word_a = word_a[0...first_index] # storing the truncated word
-      break # exits the loop
+      new_word_a = word_a.reverse!
     end
   end
 
+  # Get second word
   print "What is the second word to combine? "
   word_b = gets.chomp.downcase
   until word_b.length >= 2
     puts "Word must be greater than 1 character! Try again: "
     word_b = gets.chomp.downcase
   end
-
-  # iterate through the second
-  # initiate variable to store truncated word B
+  
+  # Truncate word_b: all letters AFTER and INCLUDING first vowel
+  # Check for vowels
   new_word_b = ""
-  second_index = 0
-
-  word_b.each_char do |character|
-    if !is_vowel?(character)
-      second_index += 1
+  word_b.each_char.with_index do |letter, index|
+    if is_vowel?(letter) == true
+      new_word_b = "#{word_b[(index + 1)..-1]}"
     else
-      new_word_b = word_b[second_index..word_b.length]
-      break
+      new_word_b = word_b
     end
   end
-  # Output by combining both truncated words
-  puts "That makes #{new_word_a}#{new_word_b}. Cool, huh!?"
+
+  portmanteau = "#{new_word_a}#{new_word_b}"
+  puts "Your new word is #{portmanteau.upcase}. Cool, huh!?"
 end
 
-# Invoke the divine power of the generator
 puts "Hello, welcome to my wild portmanteau generator!"
-
 run_generator
