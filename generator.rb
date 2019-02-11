f65a97accf17815d767a0094ec39b21f53299fca
Elle Kosling <celloelle@gmail.com>
2019-02-11 12:38:52 -0800

    Update generator.rb
    Cleaned up the file, removed some puts statements that were for testing.

--- generator.rb
+++ generator.rb
@@ -1,3 +1,4 @@
+# This is the is_vowel? method. It gets used in the run_generator method.
 def is_vowel?(letter)
   case letter
   when "a", "e", "i", "o", "u"
@@ -7,6 +8,8 @@ def is_vowel?(letter)
   end
 end
 
+# This is the portmanteau generator method.
+# The inputs are collected inside of the method (no parameters passed).
 def run_generator
   puts "Please enter the first word: "
   wordA = gets.chomp
@@ -18,7 +21,7 @@ def run_generator
   end
 
   puts "Thanks! Now enter the second word: "
-  wordB = gets.chomp
+  wordB = gets.chomp.downcase
 
   until wordB.length >= 2
     puts "Oops! This word needs to consist of at least two characters. 
@@ -26,22 +29,17 @@ def run_generator
     wordB = gets.chomp
   end
 
-  # test_vowel = "o"
-  # test_consonant = "q"
-  # puts "is #{test_vowel} a vowel?"
-  # puts is_vowel?(test_vowel)
-  # puts "Is #{test_consonant} a vowel?"
-  # puts is_vowel?(test_consonant)
+  # Instructions:
+  # In the first word, find the last instance of a vowel. The first half of the
+  # portmanteau will keep every letter of the first word until and excluding its last vowel.
 
+  # Approach:
   # calculate the first partial word by
   # finding the index of the first vowel
-  # and then generating the partial word using the
-  # indices
+  # and then generating the partial word using the indices
   # store the first partial word into variable word_part_A, class of string
-  # In the first word, find the last instance of a vowel. The first half of the portmanteau will keep every letter of the first word until and excluding its last vowel.
 
   counterA = 0
-  # rev_wordA = wordA.reverse
 
   wordA.reverse.each_char do |this_letter|
     if !is_vowel?(this_letter)
@@ -51,7 +49,6 @@ def run_generator
     end
   end
 
-  puts "wordA's first vowel when reversed is at index #{counterA}"
   word_part_A = wordA[0...((wordA.length - 1)) - counterA]
 
   # if there are no vowels in the first word, use all of its letters
@@ -59,9 +56,14 @@ def run_generator
     word_part_A = wordA
   end
 
-  puts "The first half of our portmanteau is #{word_part_A}"
+  # Instructions:
+  # In the second word, find the first instance of a vowel. The second half of the
+  # portmanteau will keep every letter of the second word after and including
+  # its first vowel.
 
-  # In the second word, find the first instance of a vowel. The second half of the portmanteau will keep every letter of the second word after and including its first vowel.
+  # Approach:
+  # do the same as word_part_A except there's no need to reverse, and the
+  # index should include the counter, which is excluded in word_part_A
   counterB = 0
 
   wordB.each_char do |this_letter|
@@ -71,7 +73,7 @@ def run_generator
       break
     end
   end
-  puts "wordB's first vowel is at index #{counterB}"
+
   word_part_B = wordB[counterB..(wordB.length)]
 
   # if there are no vowels in the second word, use all of its letters
@@ -79,14 +81,11 @@ def run_generator
     word_part_B = wordB
   end
 
-  puts "The second half of our portmanteau is #{word_part_B}"
-
   # build the portmanteau by assigning it the
-  # combination of word part A and word part B
+  # combination of word_part_A and word_part_B
 
   portmanteau = word_part_A + word_part_B
-  puts "The current portmanteau is: \n#{wordA} + #{wordB} = #{portmanteau}"
-  # puts "#{wordA} and #{wordB} in the run_generator method"
+  puts "Here is your portmanteau: \n#{wordA} + #{wordB} = #{portmanteau}"
 
   return portmanteau
 end
