63027837498c1c8d18966968b2d6dca823dd3c0d
norrise120 <46180742+norrise120@users.noreply.github.com>
2019-02-11 13:45:57 -0800

    Update generator.rb

--- generator.rb
+++ generator.rb
@@ -24,14 +24,20 @@ def run_generator
 
   puts "The first word is #{word1} and the second word is #{word2} in the run-generator method."
 
-  chunk1 = word1
+  chunk1 = word1 #establishing this here, handles cases were the word has no vowels
   word1.each_char do |c|
     if is_vowel?(c) == true
       word1_chunks = word1.split(c)
-      unless word1_chunks == []
-        chunk1 = word1.chomp(c + word1_chunks[-1])
+      if word1_chunks == [] #handles cases like "aa" where the word is just repeating same vowel
+        chunk1 = word1.chomp(c)
+      else
+        chunk1 = word1.chomp(c + word1_chunks[-1]) #handles majority of cases, removing last vowel and all letters after
       end
     end
+  end
+
+  if chunk1 == word1 && is_vowel?(chunk1[-1]) == true #handles cases where word ends in a repeating vowel (i.e. banana)
+    chunk1 = chunk1.chomp(chunk1[-1])
   end
 
   puts "Chunk 1 is #{chunk1}"
@@ -43,11 +49,11 @@ def run_generator
       unless word2_chunks == []
         chunk2 = c + word2_chunks.last
       end
-      puts "Chunk 2 is #{chunk2}"
       break
     end
   end
 
+  puts "Chunk 2 is #{chunk2}"
   puts "The Portmanteau is #{chunk1}#{chunk2}!"
 end
 
