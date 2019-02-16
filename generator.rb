7738c6400a3ea85615d8a5f12ed254b5676a7041
kaseea <kaseea@gmail.com>
2019-02-16 12:00:20 -0800

    Update generator.rb

--- generator.rb
+++ generator.rb
@@ -1,3 +1,4 @@
+
 def length_check(word)
   until word.length >= 2
     puts "please give me a word at least two characters long"
@@ -9,58 +10,81 @@ def is_vowel?(letter)
   return "aeiou".include?(letter.downcase)
 end
 
-def run_generator
+def notherVowel(string)
+  string = string.downcase
+  return string.include?(?a) || string.include?(?e) || string.include?(?i) \
+           || string.include?(?o) || string.include?(?u)
+end
+
+def firstWord()
   puts "give me the first word"
-  first = gets.chomp.to_s
-  length_check(first)
-  firstArray = []
-  first.each_char do |x|
-    firstArray << x
+  @first = gets.chomp.to_s
+  #the bit below and notherVowel method is only for the optional part, which makes some
+  #of the original code not make sense, but needed for earlier assignment paramaters
+  until notherVowel(@first)
+    puts "try the first word again, but give me a vowel"
+    @first = gets.chomp.to_s
+  end
+  length_check(@first)
+  @firstArray = []
+  @first.each_char do |x|
+    @firstArray << x
   end
   loop do
-    #could I just use later method on reversed
-    x = firstArray.slice!(-1)
+    x = @firstArray.slice!(-1)
     if is_vowel?(x)
       break
     end
-    if firstArray.length == 0
+    if @firstArray.length == 0
       break
     end
   end
-  puts firstArray
+  puts @firstArray
+end
+
+def secondWord()
   puts "give me a second word"
-  second = gets.chomp.to_s
-  length_check(second)
-  secondArray = []
-  second.each_char do |x|
-    secondArray << x
+  @second = gets.chomp.to_s
+  length_check(@second)
+  @secondArray = []
+  @second.each_char do |x|
+    @secondArray << x
   end
-  secondArray.each_with_index do |x, i|
+  @secondArray.each_with_index do |x, i|
     if is_vowel?(x)
-      secondArray.slice!(0, i)
+      @secondArray.slice!(0, i)
       break
     end
   end
-  puts "the first input is #{first}"
-  puts firstArray
-  puts "the second input is #{second}"
-  puts secondArray
+end
+
+def run_generator
+  firstWord()
+  secondWord()
+  puts "the first input is #{@first}"
+  puts "the second input is #{@second}"
   puts "in the run_generator method"
   port = ""
-  if firstArray.length == 0
-    port << first
+  if @firstArray.length == 0
+    port << @first
   else
-    firstArray.each do |x|
+    @firstArray.each do |x|
       port << x
     end
   end
-  secondArray.each do |x|
+  @secondArray.each do |x|
     port << x
   end
   puts port
 end
 
-#probable should make a quick optional method for having the program continue
-#use more helper methods
-
 run_generator
+loop do
+  puts "do you want to keep running the Portmanteau generators? y/n?"
+  answer = gets.chomp
+  if answer == "yes" || answer == "y"
+    run_generator
+  else
+    break
+  end
+end
