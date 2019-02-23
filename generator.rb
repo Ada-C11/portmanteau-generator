8ae02c4346e03eea700549582eaf893c4f6656ef
Sopheary Chiv <sophearychiv@users.noreply.github.com>
2019-02-23 11:55:33 -0800

    Update generator.rb
    removed recursive and improved code based on the feedback.

--- generator.rb
+++ generator.rb
@@ -1,25 +1,13 @@
 # check for vowels
 def is_vowel?(letter)
-  if "aeiou".include?(letter.downcase)
-    return true
-  else
-    return false
-  end
+  return "aeiou".include?(letter.downcase)
 end
 
 # validate user input
-def validate
-  input = gets.chomp.downcase
-  # allow input of at least 2 characters
-  until input.length >= 2
-    puts "Input must be at least 2 characters."
-    print "Enter again: "
-    validate
-  end
-  # optional enhancement: handle input that does not contain a vowel
-  until input =~ /["a","e","i","o","u"]/
-    print "Invalid input. Please enter a word containing at least one vowel.\nEnter again:"
-    validate
+def validate(input)
+  until input.length >= 2 && input =~ /["a","e","i","o","u"]/
+    puts "Please enter a word of at least 2 characters with at least one vowel:"
+    input = gets.chomp.downcase
   end
   return input
 end
@@ -43,12 +31,12 @@ end
 # The second half of the portmanteau will keep every letter of the second word after and including its first vowel.
 def gets_second_word(second_word)
   index = 0
-  second_word.split("").each { |char| # optional enhancement: using another method besides .each_char
+  second_word.split("").each do |char| # optional enhancement: using another method besides .each_char
     if is_vowel?(char)
       break
     end
     index += 1
-  }
+  end
   # get the second part of portmanteau
   second_part_portmanteau = second_word[index, second_word.length - index]
   return second_part_portmanteau
@@ -57,10 +45,12 @@ end
 # get the two words, combine them to create a portmanteau and print
 def run_generator
   print "Enter the first word: "
-  first_word = validate
+  input = gets.chomp.downcase
+  first_word = validate(input)
   first_part_portmanteau = gets_first_word(first_word)
   print "Enter the second word: "
-  second_word = validate
+  input = gets.chomp.downcase
+  second_word = validate(input)
   second_part_portmanteau = gets_second_word(second_word)
   puts "The first input is \"#{first_word}\""
   puts "The second input is \"#{second_word}\""
@@ -73,13 +63,15 @@ end
 def continue?
   puts "Do you want to continue? Enter \"Yes\" or \"No\" "
   answer = gets.chomp.downcase
+  until answer == "yes" || answer == "no"
+    puts "Enter \"Yes\" or \"No\""
+    answer = gets.chomp.downcase
+  end
   if answer == "yes"
     run_generator
-  elsif answer == "no"
+  else
     puts "Thanks for using the program. Goodbye!"
     exit
-  else
-    puts "Enter \"Yes\" or \"No\""
   end
 end
 
